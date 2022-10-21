3210a2aab7f66d0599571885d47a54748b9ecf96
Dusk <65917000+duskdev17@users.noreply.github.com>
2022-10-21 09:55:22 +0600

    basics
    cropping, resizing, eroding, blurring, canny edges, dialating,

--- basics.py
+++ basics.py
@@ -1,10 +1,51 @@
+from ctypes import resize
 import cv2 as cv
 
-img = cv.imread('Photos/3.jpg')
+img = cv.imread('Photos/moscow.jpg')
 cv.imshow('img', img)
 
 
-# converting to greyscale
+# converting to gayscale
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Gray', gray)
+
+
+# Blur Image
+blur = cv.GaussianBlur(img, (7, 7), cv.BORDER_DEFAULT)
+cv.imshow('blur', blur)
+
+
+# Edge Cascade
+# canny = cv.Canny(img, 125, 175)
+# cv.imshow('Canny Edges', canny)
+
+canny = cv.Canny(blur, 125, 175)
+cv.imshow('Canny Edges', canny)
+
+
+# Dialating Image
+dialated = cv.dilate(canny, (7, 7), iterations=3)
+cv.imshow('Dilated', dialated)
+
+
+# Eroding
+eroded = cv.erode(dialated,  (7, 7), iterations=3)
+cv.imshow('Eroded', eroded)
+
+
+# Resize
+resized = cv.resize(img, (1600, 1400), interpolation=cv.INTER_CUBIC)
+cv.imshow('Resized', resized)
+
+
+
+
+# Cropping
+cropped = img[150:350, 350:700]
+cv.imshow('Cropped', cropped)
+
+
+
 
 
 
