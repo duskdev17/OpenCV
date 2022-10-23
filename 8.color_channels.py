96a82b708708b0af140fc6009a63132a66bfde17
Dusk <65917000+duskdev17@users.noreply.github.com>
2022-10-23 15:17:41 +0600

    color channels

---
+++ 8.color_channels.py
@@ -0,0 +1,45 @@
+import cv2 as  cv
+import numpy as np
+
+
+img = cv.imread('Photos/forest.jpg')
+cv.imshow('Original', img)
+
+b,g,r = cv.split(img)
+
+
+cv.imshow('Blue', b)
+cv.imshow('Green', g)
+cv.imshow('Red', r)
+
+
+
+
+blank = np.zeros(img.shape[:2], dtype='uint8')
+
+
+blue = cv.merge([b,blank,blank])
+green = cv.merge([blank,g,blank])
+red = cv.merge([blank,blank,r])
+
+
+
+cv.imshow('Blue', blue)
+cv.imshow('Green', green)
+cv.imshow('Red', red)
+
+
+
+
+print(img.shape)
+print(b.shape)
+print(g.shape)
+print(r.shape)
+
+
+merged = cv.merge([b,g,r])
+cv.imshow('Merged Image', merged)
+
+
+
+cv.waitKey(0)
