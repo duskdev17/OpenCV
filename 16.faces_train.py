6c94e15e2cc16a3b4832f7ba6f26be06d9ee0586
Dusk <65917000+duskdev17@users.noreply.github.com>
2022-11-21 02:20:09 +0600

    train data

--- 16.faces_train.py
+++ 16.faces_train.py
@@ -1,3 +1,17 @@
 import os
 import cv2 as cv
+import numpy as np
 
+
+
+# p = []
+# for i in os.listdir(r'E:\OpenCV\Train'):
+#     p.append(i)
+
+# print(p)
+
+
+
+
+people = ['ben', 'bob', 'bryan']
+DIR = r'E:\OpenCV\Train'
